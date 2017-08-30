e3383406feac00cf1a1e02d9fed5a612fc55b6fb
Pedro Sousa <pjgs.sousa@gmail.com>
2017-08-30 03:49:15 +0100

    Fixed pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -5,10 +5,10 @@ source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 
 pip install \
-pandas pandas-datareader\
-matplotlib seaborn plotly\
-scikit-learn scikit-image scipy\
-jupyter\
-h5py\
-tensorflow theano keras
+  pandas pandas-datareader\
+  matplotlib seaborn plotly\
+  scikit-learn scikit-image scipy\
+  jupyter\
+  h5py\
+  tensorflow theano keras
 
